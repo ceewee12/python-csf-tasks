adac25b51a242a096458c12e496b21c919ba9d68
ceewee12 <03230030.gcbs@rub.edu.bt>
2023-10-19 21:00:01 +0600

    edited

--- task52.py
+++ task52.py
@@ -4,9 +4,7 @@ user_input = int(user_input)
 count = 1
 
 while count <= 20:
-    # count = user_input * 2
-    # print(count)
-    # break
+    
     product = count * user_input
     print(user_input, '*', count, '=', product)
     count = count + 1
